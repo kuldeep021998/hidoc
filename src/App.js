021867421948b0b82411d6d033aac6fb3321f899
KULDEEP SINGH SIKARWAR <117976159+kuldeep021998@users.noreply.github.com>
2023-05-03 20:55:19 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Footer from "./Footer";
+
 import Main from "./Main";
 
 function App() {
--- src/Footer.js
+++ src/Footer.js
@@ -3,9 +3,15 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import FolderIcon from "@mui/icons-material/Folder";
+import DescriptionIcon from "@mui/icons-material/Description";
+import VideoCameraFrontIcon from "@mui/icons-material/VideoCameraFront";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import CalculateIcon from "@mui/icons-material/Calculate";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 export default function Footer() {
   return (
-    <Grid container sx={{ backgroundColor: "blue" }}>
+    <Grid container sx={{ backgroundColor: "#130f40" }}>
       <Grid lg={3} sx={{ padding: "20px" }}>
         <img src="/assets/logo-desktop.png" style={{ height: "40px" }} />
         <div
@@ -32,7 +38,7 @@ export default function Footer() {
           <LinkedInIcon htmlColor="white" fontSize="small" />
         </div>
       </Grid>
-      <Grid lg={3} sx={{ paddingTop: "5px", paddingBottom: "30px" }}>
+      <Grid lg={3} sx={{ paddingTop: "10px", paddingBottom: "30px" }}>
         <div
           style={{
             color: "white",
@@ -54,10 +60,10 @@ export default function Footer() {
         </div>
         <div
           style={{
-            color: "#60a3bc",
+            color: "#38ada9",
             fontSize: "10px",
             marginTop: "15px",
-            width: "65%",
+            fontWeight: "bold",
           }}
         >
           Email :
@@ -74,10 +80,10 @@ export default function Footer() {
         </div>
         <div
           style={{
-            color: "#60a3bc",
+            color: "#38ada9",
             fontSize: "10px",
             marginTop: "15px",
-            width: "65%",
+            fontWeight: "bold",
           }}
         >
           Address :
@@ -101,6 +107,283 @@ export default function Footer() {
           }}
         >
           Wilmington, DE, 19808, United States
+        </div>
+      </Grid>
+
+      <Grid lg={6} sx={{ paddingTop: "10px", paddingBottom: "30px" }}>
+        <div
+          style={{
+            color: "white",
+            fontSize: "12px",
+            fontWeight: "bold",
+          }}
+        >
+          HIDOC DR. FEATURES
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            marginTop: "20px",
+          }}
+        >
+          <div
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "#dff9fb",
+              display: "flex",
+              flexDirection: "column",
+              padding: "10px",
+              width: "15%",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                padding: "5px",
+                backgroundColor: "#7ed6df",
+                borderRadius: "50px",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <FolderIcon />
+            </div>
+            <div
+              style={{ fontSize: "10px", fontWeight: "bold", color: "#22a6b3" }}
+            >
+              Drug Database
+            </div>
+          </div>
+
+          <div
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "grey",
+              display: "flex",
+              flexDirection: "column",
+              padding: "10px",
+              width: "15%",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                padding: "5px",
+                backgroundColor: "#7ed6df",
+                borderRadius: "50px",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <VideoCameraFrontIcon />
+            </div>
+            <div
+              style={{ fontSize: "10px", fontWeight: "bold", color: "#22a6b3" }}
+            >
+              Webinars
+            </div>
+          </div>
+
+          <div
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "#45aaf2",
+              display: "flex",
+              flexDirection: "column",
+              padding: "10px",
+              width: "15%",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                padding: "5px",
+                backgroundColor: "#7ed6df",
+                borderRadius: "50px",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <DescriptionIcon />
+            </div>
+            <div
+              style={{ fontSize: "10px", fontWeight: "bold", color: "#22a6b3" }}
+            >
+              Drug Interaction
+            </div>
+          </div>
+
+          <div
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "#ced6e0",
+              display: "flex",
+              flexDirection: "column",
+              padding: "10px",
+              width: "15%",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                padding: "5px",
+                backgroundColor: "#7ed6df",
+                borderRadius: "50px",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <CalculateIcon />
+            </div>
+            <div
+              style={{ fontSize: "10px", fontWeight: "bold", color: "#22a6b3" }}
+            >
+              Medical Calculator
+            </div>
+          </div>
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+          }}
+        >
+          <div
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "#F8EFBA",
+              display: "flex",
+              flexDirection: "column",
+              padding: "10px",
+              width: "15%",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                padding: "5px",
+                backgroundColor: "#7ed6df",
+                borderRadius: "50px",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <DescriptionIcon />
+            </div>
+            <div
+              style={{ fontSize: "10px", fontWeight: "bold", color: "#22a6b3" }}
+            >
+              Articles
+            </div>
+          </div>
+
+          <div
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "#9AECDB",
+              display: "flex",
+              flexDirection: "column",
+              padding: "10px",
+              width: "15%",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                padding: "5px",
+                backgroundColor: "#7ed6df",
+                borderRadius: "50px",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <EmojiEventsIcon />
+            </div>
+            <div
+              style={{ fontSize: "10px", fontWeight: "bold", color: "#22a6b3" }}
+            >
+              Quizzes
+            </div>
+          </div>
+
+          <div
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "#CAD3C8",
+              display: "flex",
+              flexDirection: "column",
+              padding: "10px",
+              width: "15%",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                padding: "5px",
+                backgroundColor: "#7ed6df",
+                borderRadius: "50px",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <DescriptionIcon />
+            </div>
+            <div
+              style={{ fontSize: "10px", fontWeight: "bold", color: "#22a6b3" }}
+            >
+              Disease Database
+            </div>
+          </div>
+
+          <div
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "grey",
+              display: "flex",
+              flexDirection: "column",
+              padding: "10px",
+              width: "15%",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                padding: "5px",
+                backgroundColor: "#7ed6df",
+                borderRadius: "50px",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <CalendarTodayIcon />
+            </div>
+            <div
+              style={{ fontSize: "10px", fontWeight: "bold", color: "#22a6b3" }}
+            >
+              Surveys
+            </div>
+          </div>
+        </div>
+      </Grid>
+      <Grid lg={12} sx={{ backgroundColor: "#95afc0", padding: "10px" }}>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <div>&copy;</div>
+          <div>Copyright 2022</div>
+          <div style={{ fontWeight: "bold", marginLeft: "2px" }}>
+            Hidoc Dr. Inc.
+          </div>
+        </div>
+        <div style={{ textAlign: "center" }}>
+          Terms & Conditions | Privacy Policy
         </div>
       </Grid>
     </Grid>
